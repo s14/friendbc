8edd689f44c03bab69105650c7acef0bd7df0ede
rbetina <rbetina@Raghus-MacBook-Pro.local>
2014-08-18 09:54:00 -0500

    Generated Comment model

---
+++ db/migrate/20140818145351_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :facebook_id
+      t.belongs_to :post, index: true
+      t.belongs_to :friend, index: true
+      t.text :message
+      t.integer :like_count
+
+      t.timestamps
+    end
+  end
+end
