a9cc1b931dc6f1813f0f6a10e71a3f5724dff7a0
rbetina <rbetina@Raghus-MacBook-Pro.local>
2014-08-18 11:41:05 -0500

    Added user_id column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818145758) do
+ActiveRecord::Schema.define(version: 20140818163740) do
 
   create_table "comments", force: true do |t|
     t.string   "facebook_id"
@@ -54,9 +54,11 @@ ActiveRecord::Schema.define(version: 20140818145758) do
     t.datetime "fb_created_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["friend_id"], name: "index_posts_on_friend_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
