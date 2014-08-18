3d6179dce3d32baf90339b086a447de101360a5d
rbetina <rbetina@Raghus-MacBook-Pro.local>
2014-08-18 09:51:04 -0500

    Revert "Generated Comment scaffold"
    This reverts commit a73c6f18c1864d05cb42f9c0b0dc8a6d5373f1a9.
    
    Conflicts:
    	config/routes.rb

--- app/views/comments/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @comment, :id, :facebook_id, :post_id, :friend_id, :message, :like_count, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :friends
 
-  resources :comments
+  resources :friends
 
   resources :posts
 
