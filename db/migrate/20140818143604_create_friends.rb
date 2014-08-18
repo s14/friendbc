d0d24d4ad4b271bc53b69a590307fb9dbe73fbeb
rbetina <rbetina@Raghus-MacBook-Pro.local>
2014-08-18 09:36:13 -0500

    Generated Friend scaffold

---
+++ db/migrate/20140818143604_create_friends.rb
@@ -0,0 +1,10 @@
+class CreateFriends < ActiveRecord::Migration
+  def change
+    create_table :friends do |t|
+      t.string :facebook_id
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
